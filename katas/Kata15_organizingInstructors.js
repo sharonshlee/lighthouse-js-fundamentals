20348e56ecb87e7a3042a50568215027432801cf
Sharon Lee <sharonshlee@icloud.com>
2021-08-03 13:40:04 -0400

    Organizing instructors based on their course.

---
+++ katas/Kata15_organizingInstructors.js
@@ -0,0 +1,37 @@
+const courseDict = ["iOS", "Web", "Blockchain"];
+
+const organizeInstructors = function (instructors) {
+    let sortedInstructors = {};
+    let webInstructor = [];
+    let iOSInstructor = [];
+    let blockChainInstructor = [];
+
+    for (const instructor of instructors) {
+        let course = instructor.course;
+        if (course === courseDict[0]) {
+            iOSInstructor.push(instructor.name);
+            sortedInstructors[course] = iOSInstructor;
+        } else if (course === courseDict[1]) {
+            webInstructor.push(instructor.name);
+            sortedInstructors[course] = webInstructor;
+        } else if (course === courseDict[2]) {
+            blockChainInstructor.push(instructor.name);
+            sortedInstructors[course] = blockChainInstructor;
+        }
+    }
+    return sortedInstructors;
+};
+
+console.log(organizeInstructors([
+    { name: "Samuel", course: "iOS" },
+    { name: "Victoria", course: "Web" },
+    { name: "Karim", course: "Web" },
+    { name: "Donald", course: "Web" }
+]));
+
+console.log(organizeInstructors([
+    { name: "Brendan", course: "Blockchain" },
+    { name: "David", course: "Web" },
+    { name: "Martha", course: "iOS" },
+    { name: "Carlos", course: "Web" }
+]));
