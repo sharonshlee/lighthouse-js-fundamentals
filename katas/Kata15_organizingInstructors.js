66549aa20fae29398df29be1fb4f1904899c1dbd
Sharon Lee <sharonshlee@icloud.com>
2021-08-15 11:52:04 -0400

    Organizing Instructors

--- katas/Kata15_organizingInstructors.js
+++ katas/Kata15_organizingInstructors.js
@@ -1,37 +1,52 @@
 const courseDict = ["iOS", "Web", "Blockchain"];
 
 const organizeInstructors = function (instructors) {
-    let sortedInstructors = {};
-    let webInstructor = [];
-    let iOSInstructor = [];
-    let blockChainInstructor = [];
+  let sortedInstructors = {};
+  let webInstructor = [];
+  let iOSInstructor = [];
+  let blockChainInstructor = [];
 
-    for (const instructor of instructors) {
-        let course = instructor.course;
-        if (course === courseDict[0]) {
-            iOSInstructor.push(instructor.name);
-            sortedInstructors[course] = iOSInstructor;
-        } else if (course === courseDict[1]) {
-            webInstructor.push(instructor.name);
-            sortedInstructors[course] = webInstructor;
-        } else if (course === courseDict[2]) {
-            blockChainInstructor.push(instructor.name);
-            sortedInstructors[course] = blockChainInstructor;
-        }
+  for (const instructor of instructors) {
+    let course = instructor.course;
+    let groupInstructors = []
+
+    if (course === courseDict[0]) {
+      iOSInstructor.push(instructor.name);
+      sortedInstructors[course] = iOSInstructor;
+    } else if (course === courseDict[1]) {
+      webInstructor.push(instructor.name);
+      sortedInstructors[course] = webInstructor;
+    } else if (course === courseDict[2]) {
+      blockChainInstructor.push(instructor.name);
+      sortedInstructors[course] = blockChainInstructor;
     }
-    return sortedInstructors;
+  }
+  return sortedInstructors;
 };
 
 console.log(organizeInstructors([
-    { name: "Samuel", course: "iOS" },
-    { name: "Victoria", course: "Web" },
-    { name: "Karim", course: "Web" },
-    { name: "Donald", course: "Web" }
+  { name: "Samuel", course: "iOS" },
+  { name: "Victoria", course: "Web" },
+  { name: "Karim", course: "Web" },
+  { name: "Donald", course: "Web" }
 ]));
 
 console.log(organizeInstructors([
-    { name: "Brendan", course: "Blockchain" },
-    { name: "David", course: "Web" },
-    { name: "Martha", course: "iOS" },
-    { name: "Carlos", course: "Web" }
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
 ]));
+
+/* Better Solution
+cconst organizeInstructors = function (instructors) {
+    let result = {};
+    instructors.forEach(function (elem) {
+        if (!result[elem.course]) {
+            result[elem.course] = [];
+        }
+        result[elem.course].push(elem.name);
+    });
+    return result;
+};
+ */
